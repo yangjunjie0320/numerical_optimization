9b3ce514272f4ac3bc3391571d23183abde10ea1
yangdatou <244140486@qq.com>
2020-08-09 22:54:12 -0500

    update

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ An implementation of the basic optimization algorithms in Julia, based on [Dr. Q
 The course mostly followed the book of [Algorithms for Optimization by Mykel J. Kochenderfer and Tim A. Wheeler](https://mitpress.mit.edu/books/algorithms-optimization).
 Here is an intersting [video](https://www.youtube.com/watch?v=ofWy5kaZU3g) by one of the authors talking about how they wrote the book in a recent Julia confernece.
 
-For some details on the optimization algorithms, we will refer to [Numerical Optimization by Jorge Nocedal and Stephen J. Wright](https://link.springer.com/book/10.1007/978-0-387-40065-5)
+For the details on the optimization algorithms, we will refer to [Numerical Optimization by Jorge Nocedal and Stephen J. Wright](https://link.springer.com/book/10.1007/978-0-387-40065-5)
---
+++ src/derivatives/dual_number.jl
@@ -0,0 +1,22 @@
+# https://en.wikipedia.org/wiki/Dual_number
+struct DualNumber <: Number  # DualNumber is a function-derivative pair
+    x::Real
+    y::Real
+end
+
+# Add the last two rules
+import Base: -,*,+, /, convert, promote_rule
+-(a::DualNumber, b::DualNumber) = DualNumber(a.x-b.x, a.y-b.y)
+*(a::DualNumber, b::DualNumber) = DualNumber(a.x*b.x, a.x*b.y+a.y*b.x)
+
++(a::DualNumber, b::DualNumber) = DualNumber(a.x+b.x, a.y+b.y)
+/(a::DualNumber, b::DualNumber) = DualNumber(a.x/b.x, (b.x*a.y - a.x*b.y)/b.x^2)
+convert(::Type{DualNumber}, x::Real) = DualNumber(x, zero(x))
+promote_rule(::Type{DualNumber}, ::Type{<:Number}) = DualNumber
+
+# Examples
+epsilon = DualNumber(0,1)
+println("epsilon                     = ", epsilon)
+println("epsilon * epsilon           = ", epsilon * epsilon)
+println("1 / epsilon                 = ", 1/(1+epsilon))
+println("(1+2*epsilon)*(3-4*epsilon) = ", (1+2*epsilon)*(3-4*epsilon))
