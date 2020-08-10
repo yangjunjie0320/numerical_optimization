5157e42502ff08c447b4c40fb1415d7ef56c8f29
yangjunjie <yangjunjie@MacBook-Pro.local>
2020-08-10 00:51:36 -0500

    update

--- src/derivatives/forward_diff.jl
+++ src/derivatives/forward_diff.jl
@@ -1,6 +1,16 @@
-using ForwardDiff
-a = ForwardDiff.Dual{Rational}(3,1)
-println("log(a^2) = ", log(a^2))
+using ForwardDiff: gradient, hessian
+f(x::Vector) = sum(sin, x) + prod(tan, x) * sum(sqrt, x);
+x = rand(5)
+println("\ngradient(f, x) = ")
+display(gradient(f, x))
+println("\n")
+println("hessian(f, x) = ")
+display(hessian(f, x))
 
-b = ForwardDiff.Dual{Rational}(2,0)
-println("log(a*b + max(a,2)) = ", log(a*b + max(a,2)))
+
+
+# a = ForwardDiff.Dual{Real}(3,1)
+# println("log(a^2) = ", log(a^2))
+
+# b = ForwardDiff.Dual{Real}(2,0)
+# println("log(a*b + max(a,2)) = ", log(a*b + max(a,2)))
